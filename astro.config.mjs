df89cafe53b344d3efcea4fdc106724a2d50c550
Alvaro Abarca <alvaroabarcadev@gmail.com>
2025-05-20 20:06:48 -0400

    Fix quotes in Astro config import statement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 // import cloudflare from "@astrojs/cloudflare";
-
 
 // https://astro.build/config
 export default defineConfig({
